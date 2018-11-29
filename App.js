95664ec9319ada55cd6daaf16e8807f0a51ba2c9
Nguyen Phuc Thien An <npta97@gmail.com>
2018-11-29 19:49:43 +0700

    Add Redux

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
+import { Provider } from 'react-redux';
 
 import AppNavigator from './navigators/AppNavigator';
+
+import store from './store';
 
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends Component {
   render() {
     return (
-      <AppContainer />
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
     );
   }
 }
