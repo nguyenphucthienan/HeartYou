5261d694b33d84158c7b18441c2033d913bf2ff5
Nguyen Phuc Thien An <npta97@gmail.com>
2018-11-29 20:27:44 +0700

    Add ReduxForm

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+// Local
+const API_URL = 'http://127.0.0.1:3000/api';
+
+// Production
+// const API_URL = 'http://heartyouapi.herokuapp.com/api';
+
+export const REGISTER_URL = `${API_URL}/auth/register`;
+export const CHECK_USERNAME_URL = `${API_URL}/auth/check_username`;
+export const LOGIN_URL = `${API_URL}/auth/login`;
+export const MY_USER_INFO_URL = `${API_URL}/auth/me`;
