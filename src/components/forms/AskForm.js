ab7b757c6745758f6c8641e5808c6ec7974f95ba
Nguyen Phuc Thien An <npta97@gmail.com>
2018-12-02 13:05:43 +0700

    Add AskForm component

--- src/components/forms/AskForm.js
+++ src/components/forms/AskForm.js
@@ -5,15 +5,15 @@ import { FormLabel, Button } from 'react-native-elements';
 
 import TextAreaInput from '../TextAreaInput';
 
-class AnswerForm extends Component {
+class AskForm extends Component {
   render() {
     const { handleSubmit, onSubmit } = this.props;
     return (
       <View>
-        <FormLabel>Enter your answer</FormLabel>
-        <Field name="answerText" component={TextAreaInput} />
+        <FormLabel>Enter your question</FormLabel>
+        <Field name="questionText" component={TextAreaInput} />
         <Button
-          title="Answer"
+          title="Ask"
           borderRadius={25}
           fontSize={14}
           fontWeight="bold"
@@ -40,18 +40,18 @@ const styles = StyleSheet.create({
 function validate(values) {
   const errors = {};
 
-  if (!values.answerText) {
-    errors.answerText = 'Please enter your answer';
+  if (!values.questionText) {
+    errors.questionText = 'Please enter your question';
   }
 
-  if (values.answerText && values.answerText.length > 500) {
-    errors.answerText = 'Answer must be a maximum of 500 characters long';
+  if (values.questionText && values.questionText.length > 500) {
+    errors.questionText = 'Question must be a maximum of 500 characters long';
   }
 
   return errors;
 }
 
 export default reduxForm({
-  form: 'answer',
+  form: 'ask',
   validate
-})(AnswerForm);
+})(AskForm);
