671f00dfc19a9cb5227f3b5e028a2e98e36e23a6
Nguyen Phuc Thien An <npta97@gmail.com>
2018-12-02 14:02:09 +0700

    Add SearchForm component

---
+++ src/components/forms/SearchForm.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Field, reduxForm } from 'redux-form';
+import { FormLabel, Button } from 'react-native-elements';
+
+import TextFieldInput from '../TextFieldInput';
+
+class SearchForm extends Component {
+  render() {
+    const { handleSubmit, onSubmit } = this.props;
+    return (
+      <View>
+        <FormLabel>Search by username</FormLabel>
+        <Field name="searchString" component={TextFieldInput} style={styles.inputFieldStyle} />
+        <Button
+          title="Search"
+          icon={{ name: 'search', type: 'feather' }}
+          onPress={handleSubmit(onSubmit)}
+          borderRadius={25}
+          fontSize={14}
+          fontWeight="bold"
+          fontFamily="monospace"
+          color="#FFF"
+          backgroundColor="#FF4081"
+          containerViewStyle={styles.containerViewStyle}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputFieldStyle: {
+    borderWidth: 2,
+    borderColor: '#03A9F4',
+    borderRadius: 25,
+    backgroundColor: '#FFF',
+    color: '#000',
+    paddingHorizontal: 20
+  },
+  containerViewStyle: {
+    marginTop: 10,
+    borderWidth: 2,
+    borderColor: '#FFF',
+    borderRadius: 25
+  }
+});
+
+function validate(values) {
+  const errors = {};
+
+  if (!values.searchString) {
+    errors.searchString = 'Please enter a search string to search';
+  }
+
+  if (values.searchString && (values.searchString.length < 3 || values.searchString.length > 20)) {
+    errors.searchString = 'Search string must be between 3 and 20 characters long';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'search',
+  validate
+})(SearchForm);
